000fc5ff248de695e462ad7fb28095588434d887
SebastianHarasim <94317328+SebastianHarasim@users.noreply.github.com>
2022-03-30 00:01:36 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Search from "./components/Search";
-
+import './App.css';
 
 const App = () => {
   return (
-    <div >
-      <Search />
+    <div className="main">
+      <div className="left">
+      <div className="left_search">
+        <Search />
+      </div>
+      </div>
+      <div className="center">
+        <button className="ui primary button">
+          Zmień
+        </button>
+      </div>
+      <div className="right">
+      <div className="right_search">
+        <Search />
+      </div>
+      </div>
+
     </div>
+
   );
 }
 
