490a8e2516e913bb95bbf0937810508949aea6bf
DominikGwizdala <93713131+DominikGwizdala@users.noreply.github.com>
2022-03-29 18:15:07 +0200

    Wersja4

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,40 @@
+import React from 'react'
+import { Dropdown } from 'semantic-ui-react'
 
-function Search (props){
-    
-    return (
-    <div>
-    <div className="ui fluid search selection dropdown">
-      <input type="hidden" name="country"/>
-        <i className="dropdown icon"></i>
-        <div className="default text">Select Country</div>
-        <div className="menu">
-          <div className="item" data-value="af"><i className="af flag"></i>Afghanistan</div>
-          <div className="item" data-value="ax"><i className="ax flag"></i>Aland Islands</div>
-          <div className="item" data-value="al"><i className="al flag"></i>Albania</div>
-          <div className="item" data-value="dz"><i className="dz flag"></i>Algeria</div>
-          <div className="item" data-value="as"><i className="as flag"></i>American Samoa</div>
-          <div className="item" data-value="ad"><i className="ad flag"></i>Andorra</div>
-        </div>
-    </div>
-  </div>
-    );
-}
+const countryOptions = [
+  { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' },
+  { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
+  { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
+  { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
+  { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
+  { key: 'ad', value: 'ad', flag: 'ad', text: 'Andorra' },
+  { key: 'ao', value: 'ao', flag: 'ao', text: 'Angola' },
+  { key: 'ai', value: 'ai', flag: 'ai', text: 'Anguilla' },
+  { key: 'ag', value: 'ag', flag: 'ag', text: 'Antigua' },
+  { key: 'ar', value: 'ar', flag: 'ar', text: 'Argentina' },
+  { key: 'am', value: 'am', flag: 'am', text: 'Armenia' },
+  { key: 'aw', value: 'aw', flag: 'aw', text: 'Aruba' },
+  { key: 'au', value: 'au', flag: 'au', text: 'Australia' },
+  { key: 'at', value: 'at', flag: 'at', text: 'Austria' },
+  { key: 'az', value: 'az', flag: 'az', text: 'Azerbaijan' },
+  { key: 'bs', value: 'bs', flag: 'bs', text: 'Bahamas' },
+  { key: 'bh', value: 'bh', flag: 'bh', text: 'Bahrain' },
+  { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh' },
+  { key: 'bb', value: 'bb', flag: 'bb', text: 'Barbados' },
+  { key: 'by', value: 'by', flag: 'by', text: 'Belarus' },
+  { key: 'be', value: 'be', flag: 'be', text: 'Belgium' },
+  { key: 'bz', value: 'bz', flag: 'bz', text: 'Belize' },
+  { key: 'bj', value: 'bj', flag: 'bj', text: 'Benin' },
+]
 
+const DropdownExampleSearchSelection = () => (
+  <Dropdown
+    placeholder='Select Country'
+    fluid
+    search
+    selection
+    options={countryOptions}
+  />
+)
 
-export default Search;
+export default DropdownExampleSearchSelection;
